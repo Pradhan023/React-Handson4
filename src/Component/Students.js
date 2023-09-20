f4fefb121c7dc3f2c88974666431b686bd0f7b66
Anish Pradhan <anishpradha5523@gmail.com>
2023-09-20 22:31:17 +0530

    Handson4

--- src/Component/Students.js
+++ src/Component/Students.js
@@ -15,14 +15,14 @@ export default function Students() {
           <td>Age</td>
           <td>Course</td>
           <td>Batch</td>
-          <td><Link className='edit'>Edit</Link></td>
+          <td><Link >Edit</Link></td>
         </tr>
         <tr>
           <td>John</td>
           <td>24</td>
           <td>Mern</td>
           <td>October</td>
-          <td><Link className='edit'>Edit</Link></td>
+          <td><Link >Edit</Link></td>
         </tr>
         
         <tr>
@@ -30,35 +30,35 @@ export default function Students() {
           <td>25</td>
           <td>Mern</td>
           <td>November</td>
-          <td><Link className='edit'>Edit</Link></td>
+          <td><Link >Edit</Link></td>
         </tr>
         <tr>
           <td>Biden</td>
           <td>24</td>
           <td>MERN</td>
           <td>January</td>
-          <td><Link className='edit'>Edit</Link></td>
+          <td><Link >Edit</Link></td>
         </tr>
         <tr>
           <td>Anish</td>
           <td>24</td>
           <td>MERN</td>
           <td>Febuary</td>
-          <td><Link className='edit'>Edit</Link></td>
+          <td><Link >Edit</Link></td>
         </tr>
         <tr>
           <td>Chris</td>
           <td>35</td>
           <td>MERN</td>
           <td>August</td>
-          <td><Link className='edit'>Edit</Link></td>
+          <td><Link >Edit</Link></td>
         </tr>
         <tr>
           <td>Anya</td>
           <td>22</td>
           <td>MERN</td>
           <td>March</td>
-          <td><Link className='edit'>Edit</Link></td>
+          <td><Link >Edit</Link></td>
         </tr>
       </tbody>
     </table>
